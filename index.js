af7be67b77531e34553ce3bb9bcd658bdc935ccb
MrChan <mrkcwayne@gmail.com>
2023-06-20 23:59:27 -0600

    added a fetch coords IP

---
+++ index.js
@@ -0,0 +1,26 @@
+// index.js
+const { fetchMyIP, fetchCoordsByIP } = require('./iss');
+
+fetchMyIP((error, ip) => {
+  if (error) {
+    console.log("It didn't work!", error);
+    return;
+  }
+
+  console.log('It worked! Returned IP:', ip);
+
+  fetchCoordsByIP(ip, (error, coords) => {
+    if (error) {
+      console.log("It didn't work!", error);
+      return;
+    }
+
+    console.log('Returned Coordinates:', coords);
+  });
+});
+
+
+
+/*Require it in index.js
+For now, call the function and pass in our (IPv4) IP address string as the first argument to the function
+For now, our callback can simply print out the values for error and data*/
